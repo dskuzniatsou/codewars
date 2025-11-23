afd7953d521a7f8d4ab4d44e126018cb3ded00e6
dskuzniatsou <sovyclub@gmail.com>
2025-11-23 15:04:49 +0300

    6kyu-23-11-2025

---
+++ 6kyu/replaceWithAlphabetPosition.js
@@ -0,0 +1,49 @@
+// 😱 DESCRIPTIONS
+
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// Input = "The sunset sets at twelve o' clock."
+// Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+// 🐱‍👤 TESTS
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+// Test.assertEquals(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+//   });
+// });
+
+
+
+
+// ✔ SOLUTION
+
+// my solution
+function alphabetPosition(text) {
+ return text.toLowerCase().replace(/[^a-z]/g, '').split('').map(char=> char.charCodeAt(0) - 96).join(' ')
+}
+
+
+
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+
+//BEST PRACTICES
+// function alphabetPosition(text) {
+//   var result = "";
+//   for (var i = 0; i < text.length; i++){
+//     var code = text.toUpperCase().charCodeAt(i)
+//     if (code > 64 && code < 91) result += (code - 64) + " ";
+//   }
+
+//   return result.slice(0, result.length-1);
+// }
